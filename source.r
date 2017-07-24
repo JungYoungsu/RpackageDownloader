4317e7b4a96990bdc5f53e320aede95a81992b31
JungYoungsu <terun707@naver.com>
2017-07-24 10:57:42 +0900

    code cleanup

--- source.r
+++ source.r
@@ -1,10 +1,3 @@
-list.of.packages <- c("rvest", "magrittr")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
-library(rvest)
-library(magrittr)
-
 ###### Setting ######
 # example package : bcROCsurface
 target_packageName <- "bcROCsurface"
@@ -18,46 +11,43 @@ dr <- "C:/zips/"
 repo <- "http://cran.nexr.com/bin/windows/contrib/3.4/"
 ###### ####### ######
 
+list.of.packages <- c("rvest", "magrittr")
+new.packages <- list.of.packages[!(list.of.packages 
+                                   %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library(rvest)
+library(magrittr)
+
+
 packages <<- c(target_packageName)
 zips <<- character()
 
 addPackages <- function(pkg_name) {
-  address <- paste0("https://cran.r-project.org/web/packages/", paste0(pkg_name, "/index.html"))
-  doc <- read_html(address)
+  doc <- read_html(paste0("https://cran.r-project.org/web/packages/"
+                          , paste0(pkg_name, "/index.html")))
   tds <- doc %>% html_nodes("table") %>% html_nodes("td")
   
   for ( i in 1:(tds %>% length()) ) {
     td <- tds %>% extract(i) %>% html_text()
-    if ( td == "Imports:") {
+    if ( td == "Imports:" || td == "Depends:") {
       import_pkgs <- tds %>% extract(i+1) %>% html_nodes("a")
       
       for ( pkg in import_pkgs ) {
-        pkg_txt <- pkg %>% html_text()
-        if ( ! pkg_txt %in% packages ) {
-          packages <<- c(packages, pkg_txt)
-          addPackages(pkg_txt)
-        }
-      }
-    }
-    else if ( td == "Depends:") {
-      import_pkgs <- tds %>% extract(i+1) %>% html_nodes("a")
-      
-      for ( pkg in import_pkgs ) {
-        pkg_txt <- pkg %>% html_text()
-        if ( ! pkg_txt %in% packages ) {
-          packages <<- c(packages, pkg_txt)
-          addPackages(pkg_txt)
+        pk <- pkg %>% html_text()
+        if ( ! pk %in% packages ) {
+          packages <<- c(packages, pk)
+          addPackages(pk)
         }
       }
     }
     else if ( grepl ("r-release: ", td) ) {
       rvec <- strsplit(td, ", ")
-      zips <<- c(zips, gsub("r-release: ", "", rvec[[1]] [ grepl("r-release: ", rvec[[1]]) ] ))
+      zips <<- c(zips, gsub("r-release: ", "",
+                            rvec[[1]] [ grepl("r-release: ", rvec[[1]]) ] ))
       break
     }
   }
 }
-
 
 addPackages(target_packageName)
 
